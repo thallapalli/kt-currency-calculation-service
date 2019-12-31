d03bdbb51b3f2531b4098c77b911365bbd75900a
karnakar <karnakar@ymail.com>
2019-12-31 00:37:54 -0500

    First commit

---
+++ src/main/java/kt/springboot/currency/calculation/service/KtCurrencyCalculationServiceApplication.java
@@ -0,0 +1,13 @@
+package kt.springboot.currency.calculation.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtCurrencyCalculationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtCurrencyCalculationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kt/springboot/currency/calculation/service/KtCurrencyCalculationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kt.springboot.currency.calculation.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtCurrencyCalculationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
